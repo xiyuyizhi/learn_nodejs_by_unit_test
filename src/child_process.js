ab42a32d3a0472b97933fe424fa8da3d27d8dcca
xiyuyizhi <1553810509@qq.com>
2018-08-11 10:22:42 +0800

    close IPC channel

--- src/child_process.js
+++ src/child_process.js
@@ -1,48 +1,61 @@
 switch (process.argv[2]) {
-    case "signal":
-        signal();
-        break;
-    case "killChild":
-        killChild();
-        break;
-    case "notEmitWhenHaveMessage":
-        notEmitWhenHaveMessage();
-        break;
-    case "silent":
-        silent();
-        break;
-    case "options_stdio":
-        options_stdio();
-        break;
+  case 'signal':
+    signal();
+    break;
+  case 'killChild':
+    killChild();
+    break;
+  case 'notEmitWhenHaveMessage':
+    notEmitWhenHaveMessage();
+    break;
+  case 'silent':
+    silent();
+    break;
+  case 'options_stdio':
+    options_stdio();
+    break;
+  case 'closeChannel':
+    closeChannel();
+    break;
 }
 
 function signal() {
-    process.send(process.pid);
+  process.send(process.pid);
 }
 
 function killChild() {
-    const timer = setInterval(() => {}, 300);
+  const timer = setInterval(() => {}, 300);
 
-    process.on("SIGTERM", () => {
-        // process.exit();
-        clearInterval(timer);
-    });
+  process.on('SIGTERM', () => {
+    // process.exit();
+    clearInterval(timer);
+  });
 }
 function notEmitWhenHaveMessage() {
-    const timer = setInterval(() => {}, 300);
-    process.on("SIGTERM", () => {
-        process.exit();
-        // clearInterval(timer); // not effect
-    });
-    process.on("message", msg => {
-        clearInterval(timer); // not effect
-    });
+  const timer = setInterval(() => {}, 300);
+  process.on('SIGTERM', () => {
+    process.exit();
+    // clearInterval(timer); // not effect,can not exit process
+  });
+  process.on('message', msg => {
+    clearInterval(timer); // not effect,can not exit process
+  });
 }
 
 function silent() {
-    console.log("log_to_parent_not_to_terminal");
+  console.log('log_to_parent_not_to_terminal');
 }
 
 function options_stdio() {
-    throw new Error("error");
+  throw new Error('error');
+}
+
+function closeChannel() {
+  const timer = setInterval(() => {}, 300);
+
+  process.on('SIGTERM', () => {
+    clearInterval(timer);
+  });
+
+  process.on('message', () => {});
 }
--- test/child_process.spec.js
+++ test/child_process.spec.js
@@ -1,111 +1,120 @@
-const assert = require("assert");
-const path = require("path");
-const fs = require("fs");
-const { spawn, exec, fork } = require("child_process");
-const SHELL_NAME = "child_process.js";
+const assert = require('assert');
+const path = require('path');
+const fs = require('fs');
+const { spawn, exec, fork } = require('child_process');
+const { resolvePath, translateArrStrToArr } = require('../util/util');
+const SHELL_NAME = resolvePath('child_process.js');
 
-const { resolvePath, translateArrStrToArr } = require("../util/util");
+describe('#Child_Process', function() {
+  this.timeout(5000);
 
-describe("#Child_Process", function() {
-    this.timeout(5000);
+  it('test process.send(),process.on("message") ', done => {
+    const subprocess = fork(SHELL_NAME, ['signal']);
+    subprocess.on('message', pid => {
+      assert.equal(subprocess.pid, pid);
+      done();
+    });
+  });
 
-    it('test process.send(),process.on("message") ', done => {
-        const subprocess = fork(resolvePath(SHELL_NAME), ["signal"]);
-        subprocess.on("message", pid => {
-            assert.equal(subprocess.pid, pid);
-            done();
-        });
+  it('test kill child process by clearInterval or process.exit() ', done => {
+    const subprocess = fork(SHELL_NAME, ['killChild'], {
+      stdio: ['inherit', 'pipe', 'inherit', 'ipc']
+    });
+    assert.equal(subprocess.connected, true);
+    subprocess.stdout.on('data', data => {
+      assert.equal(subprocess.pid + '_exit\n', data.toString());
+    });
+    setTimeout(() => {
+      subprocess.kill();
+      // subprocess.send("exit");
+    }, 200);
+    setTimeout(() => {
+      assert.equal(subprocess.connected, false);
+      done();
+    }, 500);
+  });
+
+  it('test child process not exit when have message Event and not explicit call process.exit() ', done => {
+    const subprocess = fork(SHELL_NAME, ['notEmitWhenHaveMessage']);
+    assert.equal(subprocess.connected, true);
+    setTimeout(() => {
+      subprocess.send('exit'); //not effect
+    }, 200);
+    setTimeout(() => {
+      assert.equal(subprocess.connected, true);
+    }, 300);
+    setTimeout(() => {
+      assert.equal(subprocess.connected, true);
+      subprocess.kill();
+    }, 400);
+    setTimeout(() => {
+      assert.equal(subprocess.connected, false);
+      done();
+    }, 500);
+  });
+
+  it('test fork() with options.silent = true', done => {
+    const subprocess = fork(SHELL_NAME, ['silent'], {
+      silent: true
+    });
+    assert.ok(subprocess.stdout);
+    subprocess.stdout.on('data', data => {
+      const result = translateArrStrToArr(data.toString());
+      assert.equal(result[0], 'log_to_parent_not_to_terminal');
+      done();
+    });
+  });
+
+  it('test setting options.stdio', done => {
+    const subprocess = fork(SHELL_NAME, ['options_stdio'], {
+      stdio: ['pipe', 'inherit', 'pipe', 'ipc'] // stdin pipe parent process , stdout、stderr继承parent,打印到terminal
     });
 
-    it("test kill child process by clearInterval or process.exit() ", done => {
-        const subprocess = fork(resolvePath(SHELL_NAME), ["killChild"], {
-            stdio: ["inherit", "pipe", "inherit", "ipc"]
-        });
-        assert.equal(subprocess.connected, true);
-        subprocess.stdout.on("data", data => {
-            assert.equal(subprocess.pid + "_exit\n", data.toString());
-        });
-        setTimeout(() => {
-            subprocess.kill();
-            // subprocess.send("exit");
-        }, 200);
-        setTimeout(() => {
-            assert.equal(subprocess.connected, false);
-            done();
-        }, 500);
+    assert.ok(subprocess.stdin);
+    assert.equal(subprocess.stdout, null);
+    subprocess.stderr.on('data', data => {
+      assert.ok(data);
+      done();
+    });
+  });
+
+  it('test closes the IPC channel between parent and child', done => {
+    const subprocess = fork(SHELL_NAME, ['closeChannel']);
+    assert.equal(subprocess.connected, true);
+
+    setTimeout(() => {
+      subprocess.kill();
+    }, 100);
+
+    setTimeout(() => {
+      subprocess.disconnect(); // 关闭父子间IPC通道
+      assert.equal(subprocess.connected, false);
+      done();
+    }, 300);
+  });
+
+  describe('## stdout to file', () => {
+    after(function() {
+      fs.unlinkSync('test/stdout.txt');
+      fs.unlinkSync('test/stderr.txt');
     });
 
-    it("test child process not exit when have message Event and not explicit call process.exit() ", done => {
-        const subprocess = fork(resolvePath(SHELL_NAME), [
-            "notEmitWhenHaveMessage"
-        ]);
-        assert.equal(subprocess.connected, true);
-        setTimeout(() => {
-            subprocess.send("exit"); //not effect
-        }, 200);
-        setTimeout(() => {
-            assert.equal(subprocess.connected, true);
-        }, 300);
-        setTimeout(() => {
-            assert.equal(subprocess.connected, true);
-            subprocess.kill();
-        }, 400);
-        setTimeout(() => {
-            assert.equal(subprocess.connected, false);
-            done();
-        }, 500);
+    it('test write child process stdout to file', done => {
+      spawn('ls', ['/noExitDir'], {
+        stdio: [
+          'pipe',
+          fs.openSync('test/stdout.txt', 'w'),
+          fs.openSync('test/stderr.txt', 'w')
+          // process.stderr
+        ]
+      });
+      setTimeout(() => {
+        const exits = fs.existsSync('test/stdout.txt');
+        assert.ok(exits);
+        const err = fs.readFileSync('test/stderr.txt', 'utf8');
+        assert.equal(err, 'ls: /noExitDir: No such file or directory\n');
+        done();
+      }, 100);
     });
-
-    it("test fork() with options.silent = true", done => {
-        const subprocess = fork(resolvePath(SHELL_NAME), ["silent"], {
-            silent: true
-        });
-        assert.ok(subprocess.stdout);
-        subprocess.stdout.on("data", data => {
-            const result = translateArrStrToArr(data.toString());
-            assert.equal(result[0], "log_to_parent_not_to_terminal");
-            done();
-        });
-    });
-
-    it("test setting options.stdio", done => {
-        const subprocess = fork(resolvePath(SHELL_NAME), ["options_stdio"], {
-            stdio: ["pipe", "inherit", "pipe", "ipc"] // stdin pipe parent process , stdout、stderr继承parent,打印到terminal
-        });
-
-        assert.ok(subprocess.stdin);
-        assert.equal(subprocess.stdout, null);
-        subprocess.stderr.on("data", data => {
-            assert.ok(data);
-            done();
-        });
-    });
-
-    describe("## stdout to file", () => {
-        after(function() {
-            fs.unlinkSync("test/stdout.txt");
-            fs.unlinkSync("test/stderr.txt");
-        });
-
-        it("test write child process stdout to file", done => {
-            spawn("ls", ["/noExitDir"], {
-                stdio: [
-                    "pipe",
-                    fs.openSync("test/stdout.txt", "w"),
-                    fs.openSync("test/stderr.txt", "w")
-                    // process.stderr
-                ]
-            });
-            setTimeout(() => {
-                const exits = fs.existsSync("test/stdout.txt");
-                assert.ok(exits);
-                const err = fs.readFileSync("test/stderr.txt", "utf8");
-                assert.equal(
-                    err,
-                    "ls: /noExitDir: No such file or directory\n"
-                );
-                done();
-            }, 100);
-        });
-    });
+  });
 });
