cbbca77d85788ee6bad053fffba9d742be6f2b95
xiyuyizhi <1553810509@qq.com>
2018-08-28 10:52:18 +0800

    error

--- src/error.js
+++ src/error.js
@@ -1,3 +1,8 @@
+//
+//
+//
+const EventEmitter = require('events');
+
 if (process.argv[2] == 'uncaughtException') {
   process.on('uncaughtException', error => {
     console.log(error.message);
@@ -6,5 +11,44 @@ if (process.argv[2] == 'uncaughtException') {
   throw new Error('handleErrorInuncaughtException');
 }
 
+if (process.argv[2] == 'uncaughtException_Async') {
+  process.on('uncaughtException', error => {
+    console.log(error.message);
+  });
+
+  setTimeout(() => {
+    console.log(a);
+  }, 1000);
+}
+
+if (process.argv[2] == 'tryCatch') {
+  process.on('uncaughtException', error => {
+    console.log('handle in uncaughtException：' + error.message);
+  });
+
+  try {
+    console.log(a);
+  } catch (e) {
+    console.log('handle in catch：' + e.message);
+    setTimeout(() => {
+      throw new Error('async error');
+    }, 1000);
+  }
+}
+
 if (process.argv[2] == 'handleByErrorEvent') {
+  class CustomE extends EventEmitter {}
+  const cusE = new CustomE();
+
+  cusE.addListener('event1', () => {
+    setTimeout(() => {
+      cusE.emit('error', new Error('emit error'));
+    });
+  });
+
+  cusE.on('error', error => {
+    process.send('in error event：' + error.message);
+  });
+
+  cusE.emit('event1');
 }
--- test/error.spec.js
+++ test/error.spec.js
@@ -1,16 +1,15 @@
 const assert = require('assert');
 const path = require('path');
 const { fork } = require('child_process');
+const shellPath = path.resolve(__dirname, '../src/error.js');
 
-describe('#Error', () => {
+describe('#Error', function() {
+  this.timeout(5000);
+
   it('test handle error by process.on("uncaughtException")', done => {
-    const sub = fork(
-      path.resolve(__dirname, '../src/error.js'),
-      ['uncaughtException'],
-      {
-        stdio: ['pipe', 'pipe', 'pipe', 'ipc']
-      }
-    );
+    const sub = fork(shellPath, ['uncaughtException'], {
+      stdio: ['pipe', 'pipe', 'pipe', 'ipc']
+    });
     sub.stdout.setEncoding('utf8');
     sub.stdout.on('data', data => {
       assert.equal(data, 'handleErrorInuncaughtException\n');
@@ -18,13 +17,39 @@ describe('#Error', () => {
     });
   });
 
-  it('test handle error by error Event of EventEmitter', () => {
-    const sub = fork(
-      path.resolve(__dirname, '../src/error.js'),
-      ['handleByErrorEvent'],
-      {
-        stdio: ['pipe', 'pipe', 'pipe', 'ipc']
+  it('test handle error by process.on("uncaughtException") with async', done => {
+    const sub = fork(shellPath, ['uncaughtException_Async'], {
+      stdio: ['pipe', 'pipe', 'pipe', 'ipc']
+    });
+    sub.stdout.setEncoding('utf8');
+    sub.stdout.on('data', data => {
+      assert.equal(data, 'a is not defined\n');
+      done();
+    });
+  });
+
+  it('test catch error with try catch', done => {
+    const sub = fork(shellPath, ['tryCatch'], {
+      stdio: ['pipe', 'pipe', 'pipe', 'ipc']
+    });
+    sub.stdout.setEncoding('utf8');
+    let first = true;
+    sub.stdout.on('data', data => {
+      if (first) {
+        first = false;
+        assert.equal(data, 'handle in catch：a is not defined\n');
+      } else {
+        assert.equal(data, 'handle in uncaughtException：async error\n');
+        done();
       }
-    );
+    });
+  });
+
+  it('test handle error by error Event of EventEmitter', done => {
+    const sub = fork(shellPath, ['handleByErrorEvent']);
+    sub.on('message', data => {
+      assert.equal(data, 'in error event：emit error');
+      done();
+    });
   });
 });
